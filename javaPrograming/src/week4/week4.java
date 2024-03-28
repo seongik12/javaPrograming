192812c2dabab3ea6714f8b361f5721d199eb342
seongik12 <jaraka@u1.ac.kr>
2024-03-28 13:20:30 +0900

    week4

---
+++ javaPrograming/src/week4/week4.java
@@ -0,0 +1,38 @@
+package week4;
+
+public class week4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println(69/10);
+//		System.out.println(69%10);
+		
+		int n = 5;
+		int r = n % 2;
+//		System.out.println("r : " + r);
+		
+//		System.out.println(1 / 2);
+//		System.out.println(1 % 2);
+		
+		
+		int i = 1;
+		int j = 5;
+		i=i*3;
+		j=i;
+		
+		System.out.println("i : " + i);
+		System.out.println("j : " + j);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
